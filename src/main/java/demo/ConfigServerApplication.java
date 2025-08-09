5826616e1670551931fd850fbb23054d3a4544fd
Steven Squall <zoukailiang0@163.com>
2025-08-09 22:07:34 +0800

    implement jdbc+git config server

--- src/main/java/demo/ConfigServerApplication.java
+++ src/main/java/demo/ConfigServerApplication.java
@@ -2,9 +2,16 @@ package demo;
 
 
 import lombok.extern.slf4j.Slf4j;
+//import org.springframework.boot.ApplicationRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.config.server.EnableConfigServer;
+//import org.springframework.cloud.config.server.environment.EnvironmentRepository;
+//import org.springframework.cloud.config.server.environment.JdbcEnvironmentRepository;
+//import org.springframework.context.annotation.Bean;
+//import org.springframework.context.annotation.Import;
+//import org.springframework.core.Ordered;
+//import java.util.List;
 
 
 @SpringBootApplication
@@ -20,4 +27,30 @@ public class ConfigServerApplication {
         log.info("main -> started ConfigServerApplication");
     }
 
+/*
+    @Bean
+    public ApplicationRunner jdbcDebugRunner(EnvironmentRepository repo) {
+        return args -> {
+            log.info("=== CONFIG REPOSITORY TYPE ===");
+            log.info("Repository class: {}", repo.getClass().getName());
+
+            if (repo instanceof JdbcEnvironmentRepository) {
+                log.info("JDBC repository is ACTIVE");
+            } else {
+                log.warn("JDBC repository is NOT active");
+            }
+        };
+    }
+
+
+    @Bean
+    public ApplicationRunner repositoryChecker(List<EnvironmentRepository> repos) {
+        return args -> {
+            log.info("=== ACTIVE CONFIG REPOSITORIES ===");
+            repos.forEach(repo ->
+                log.info("Repository: {}", repo.getClass().getSimpleName())
+            );
+        };
+    }
+*/
 }
